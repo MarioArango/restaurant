051c8253b29ad64bdbd1e096541f601f6b346959
Mario Arango <mlimfx@gmail.com>
2022-07-12 00:31:06 -0500

    avance 9

--- src/routes/Home/OrderSummary.js
+++ src/routes/Home/OrderSummary.js
@@ -77,6 +77,27 @@ const OrderSummary = (props) => {
         onClose={handleCancel}
         visible={visibleOrder}
         key="right"
+        footer={
+            <div>
+                <div className='flex justify-between px-2 mb-3'>
+                    <div className='font-bold text-xl'>TOTAL:</div>
+                    <div className='font-bold text-xl'>S/. {calculatePriceTotalOrder() ? currency.format(Number(calculatePriceTotalOrder()), currencyFE) : '0.00'}</div>
+                </div>
+                <Col span={24}>
+                    <Popconfirm placement="top" title='¿Desea generar el pedido?' onConfirm={handleSendOrder} okText="Sí" cancelText="No">
+                        <Button
+                            type="primary" 
+                            icon={<SendOutlined />}
+                            loading={loadingGenOrder}
+                            className="bg-primary"
+                            block
+                        >
+                            Enviar
+                        </Button>
+                    </Popconfirm>
+                </Col>
+            </div>
+        }
       >
         <Row gutter={12}>
             <Col span={24}>
@@ -85,68 +106,51 @@ const OrderSummary = (props) => {
                     itemLayout="horizontal"
                     dataSource={orders}
                     renderItem={(dish, index) => (
-                        <List.Item
-                            key={index}
-                            actions={[
-                                <Button 
-                                    shape="circle" 
-                                    icon={<MinusOutlined/>} 
-                                    onClick={() => handleDelQtyDish(dish)} 
-                                    key="del"
-                                />,
-                                <Button 
-                                    type="primary"
-                                    className='bg-primary'
-                                    shape="circle" 
-                                    icon={<PlusOutlined/>} 
-                                    onClick={() => handleAddQtyDish(dish)} 
-                                    key="add"
-                                />,
-                                <Popconfirm 
-                                    placement="left" 
-                                    title='¿Eliminar plato?' 
-                                    onConfirm={() => handleDelTotalQtyDish(dish)} 
-                                    okText="Sí"
-                                    cancelText="No"
-                                >
-                                    <Button 
-                                        shape="circle" 
-                                        icon={ <DeleteTwoTone twoToneColor="#ed4956"/>}
-                                        key="del-total"
-                                    />
-                                </Popconfirm>
-                            ]}
-                        >
+                        <List.Item key={index}>
                             <Skeleton avatar title={false} loading={false} active>
                                 <List.Item.Meta
                                     avatar={<Avatar src={dish.sPhoto} size="large" />}
-                                    title={dish.sName}
+                                    title={<strong>{dish.sName}</strong>}
                                     description={quantityByDish(dish) + "und."}
                                 />
                                 <div>S/. {dish.nPrice ? currency.format(calculatePriceTotalByDish(dish), currencyFE) : '0.00'}</div>
+                                <div className='flex justify-between'>
+                                    <Button 
+                                        className='mx-2'
+                                        shape="circle" 
+                                        icon={<MinusOutlined/>} 
+                                        onClick={() => handleDelQtyDish(dish)} 
+                                        key="del"
+                                    />
+                                    <Button 
+                                        type="primary"
+                                        className='bg-primary mr-2'
+                                        shape="circle" 
+                                        icon={<PlusOutlined/>} 
+                                        onClick={() => handleAddQtyDish(dish)} 
+                                        key="add"
+                                    />
+                                    <Popconfirm 
+                                        placement="left" 
+                                        title='¿Eliminar plato?' 
+                                        onConfirm={() => handleDelTotalQtyDish(dish)} 
+                                        okText="Sí"
+                                        cancelText="No"
+                                    >
+                                        <Button 
+                                            shape="circle" 
+                                            icon={ <DeleteTwoTone twoToneColor="#ed4956"/>}
+                                            key="del-total"
+                                        />
+                                    </Popconfirm>
+                                </div>
                             </Skeleton>
                         </List.Item>
                     )}
                 />
             </Col>
             <Divider/>
-            <div className='flex justify-between w-screen px-2 mb-3'>
-                <div className='font-bold text-xl'>TOTAL:</div>
-                <div className='font-bold text-xl'>S/. {calculatePriceTotalOrder() ? currency.format(Number(calculatePriceTotalOrder()), currencyFE) : '0.00'}</div>
-            </div>
-            <Col span={24}>
-                <Popconfirm placement="top" title='¿Desea generar el pedido?' onConfirm={handleSendOrder} okText="Sí" cancelText="No">
-                    <Button
-                        type="primary" 
-                        icon={<SendOutlined />}
-                        loading={loadingGenOrder}
-                        className="bg-primary"
-                        block
-                    >
-                        Enviar
-                    </Button>
-                </Popconfirm>
-            </Col>
+            
         </Row>
       </Drawer>
   )
